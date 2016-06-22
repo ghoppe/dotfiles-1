4e83720c50398536e654b0f0b6f34d8ea70377b7
Glenn Hoppe <ghoppe@gmail.com>
2016-06-22 15:00:05 -0600

    New autojump stuff

---
+++ zsh/autojump.zsh
@@ -0,0 +1,7 @@
+#  Autojump Support
+#
+#  brew install Autojump
+#
+#  https://github.com/wting/autojump
+
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
